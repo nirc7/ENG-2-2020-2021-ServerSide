05530eefa88ffddb0262b8431cbe36c5fdba7a07
nirc7 <i2cs2013@gmail.com>
2021-03-16 12:49:16 +0200

    web api1

---
+++ 05 16-03-2021 Web API/WebApplication1/WebApplication1/Controllers/StudentsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class StudentsController : ApiController
+    {
+        //api/students
+        public IEnumerable<Student> Get() 
+        {
+            return StudentsDbMOCK.students;
+        }
+
+        //api/students/2
+        public Student Get(int id)
+        {
+            //StudentsDbMOCK.students.Where(stu=> stu.Name.Contains("avi")); //return a list
+            return StudentsDbMOCK.students.SingleOrDefault(stu => stu.ID == id); //return the first
+        }
+    }
+}
